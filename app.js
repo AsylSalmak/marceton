d240cdb37613069e9504a87d16bd531f25dd60dc
Abdygapparov-devpro <yerden.abdygapparov@dev.pro>
2022-05-05 01:27:18 +0600

    .

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-// if (process.env.NODE_ENV === 'production') {
 app.use(express.static('build'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
-// }
 
 app.listen(port);
